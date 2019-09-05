1cb0e192a89fc44ad4bd093851d4e0e2d2ddb10b
valentinbdv <valentin.pichard@bondevisite.fr>
2019-09-05 23:43:58 +0200

    Removed build files

--- Inputs/checkbox.d.ts
+++
@@ -1,7 +0,0 @@
-import { Input, inputEvents } from './input';
-export declare class Checkbox extends Input {
-    constructor(parent: HTMLElement, label: string, checked: boolean);
-    setValue(checked: boolean): void;
-    inputEvent: inputEvents;
-    on(event: string, funct: Function): this;
-}
