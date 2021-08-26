42c193d2a34e034838344afaeedf06a8f005b518
pichou <pichou@naker.io>
2021-08-26 15:21:53 +0200

    update modal close button design

--- Layers/modal.ts
+++ Layers/modal.ts
@@ -18,18 +18,21 @@ export class Modal {
 
     constructor(title: string, description?: string, clas?: string) {
         let modalclass = (clas) ? clas : '';
+
+        let closeButton: HTMLElement
         // We use a form to make sure we have no autocompletion
         this.control = el('form.modal.' + modalclass, { autocomplete: "off", onsubmit: (evt) => { evt.preventDefault(); } },
             [
                 el('div.modal-close.button-neumorphisme', { onclick: (() => { this.backgroundClick() }) },
-                    el('div.button-shadow',
-                        el('div.close', 'x')
-                    )
+                    closeButton = el('div.button-shadow')
                 ),
                 this.title = el('div.modal-title'),
                 this.description = el('div.modal-description', description),
             ]
         );
+
+        closeButton.innerHTML = '<svg aria-hidden="true" focusable="false" viewBox="0 0 24 24" role="img" width="20px" height="20px" fill="none"><path stroke="currentColor" stroke-width="1.5" d="M18.972 5.027L5.027 18.972m0-13.945l13.945 13.945"></path></svg>'
+
         if (!title) setStyle(this.title, { display: 'none' });
         else this.title.innerHTML = title;
         if (!description) setStyle(this.description, { display: 'none' });
